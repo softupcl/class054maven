6f12bedc0c3d4a0f60168f594a40fd43d752d8ad
danielfarias82 <44758415+danielfarias82@users.noreply.github.com>
2023-05-29 01:02:34 -0600

    Maven First

---
+++ src/main/java/com/example/demo/acdata/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.acdata;
+
+
+	import org.springframework.data.repository.CrudRepository;
+
+	import com.example.demo.acdata.*;
+
+	// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+	// CRUD refers Create, Read, Update, Delete
+
+	public interface UserRepository extends CrudRepository<User, Integer> {
+
+	}
+
